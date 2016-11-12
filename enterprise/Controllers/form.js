485d4f4869f777c71d66466a17ddce643c98c21b
dwoldenberg1 <dwoldenberg1@gmail.com>
2016-11-12 17:35:35 -0500

    added graph implementation

--- enterprise/Controllers/form.js
+++ enterprise/Controllers/form.js
@@ -19,41 +19,66 @@ angular.module('userForm', [])
 
 var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {
-        type: 'ar',
+        type: 'pie',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: ["Food", "Necessities", "Entertainment"],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
+                data: [30, 60, 10],
+            backgroundColor: [
+                "#c02dff",
+                "#f97b36",
+                "#368bf9"
+            ],
+            hoverBackgroundColor: [
+                "#c02dff",
+                "#f97b36",
+                "#368bf9"
+            ]}]
         }
     });
+
+var ctx = document.getElementById("myChart2");
+    var myChart2 = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Ideal", "Actual", "Ideal", "Actual", "Ideal", "Actual"],
+            datasets: [
+                {
+                    label: "Your Progress",
+                    //Color is either red ("#ff4f14") or green (#13d65d)
+                    backgroundColor: [
+                        "#c02dff",
+                        "#ff4f14",
+                        "#f97b36",
+                        "#13d65d",
+                        "#368bf9",
+                        "#ff4f14"
+                    ],
+                    borderColor: [
+                        "#c02dff",
+                        "#ff4f14",
+                        "#f97b36",
+                        "#13d65d",
+                        "#368bf9",
+                        "#ff4f14"
+                    ],
+                    borderWidth: 1,
+                    data: [30, 55, 50, 35, 10, 15],
+                }
+            ]
+        },
+        options: {
+        scales: {
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    }
+    });
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
 
 /*** jquery Updating ***/
 
