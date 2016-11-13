2208b56d847063e801563cf6872cf8c7e0c5013f
dwoldenberg1 <dwoldenberg1@gmail.com>
2016-11-13 04:43:46 -0500

    plz be it

--- enterprise/Controllers/form.js
+++ enterprise/Controllers/form.js
@@ -20,6 +20,8 @@ angular.module('userForm', [])
                           });
             //localStorage.setItem("budget",budget);
             //console.log(budget);
+            var socket = io.connect('http://localhost:5000');
+
             socket.emit('userinput',budget2);
             var ctxPie2 = document.getElementById("idealPie");
             //console.log($scope.food);
@@ -49,7 +51,6 @@ angular.module('userForm', [])
                 console.log($scope.budget);
                 //$scope.user = angular.copy($scope.master);
             }; */
-          var socket = io.connect('http://localhost:5000');
           socket.emit('nextday');
           socket.on('parsedmint', function (data) {
           $scope.category = data;
